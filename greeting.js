592fe5fa1594c2b472049846468472835a926d91
jeeyeon <sdc9141@gmail.com>
2020-06-14 23:57:43 +0900

    modify css
    modify css

--- greeting.js
+++ greeting.js
@@ -1,6 +1,8 @@
 const form = document.querySelector(".js-form"),
     input = form.querySelector("input"),
-    greeting = document.querySelector(".js-greetings");
+    greeting = document.querySelector(".js-greetings"),
+    clock = document.querySelector(".js-clock"),
+    toDo = document.querySelector(".js-toDoForm");
 
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
@@ -25,7 +27,9 @@ function askForName() {
 function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
-    greeting.innerHTML = `Hello ${text}`;
+    clock.classList.add(SHOWING_CN);
+    toDo.classList.add(SHOWING_CN);
+    greeting.innerHTML = `Hello, ${text}.`;
 }
 
 function loadNmae() {
